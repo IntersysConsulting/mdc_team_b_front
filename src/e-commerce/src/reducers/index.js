3f4af26b4d1b521e7c56edc59ec1b8ad82615f7c
Christian <christianahvilla@gmail.com>
2019-09-06 14:21:27 -0500

    fix typo

--- src/e-commerce/src/reducers/index.js
+++ src/e-commerce/src/reducers/index.js
@@ -6,10 +6,10 @@ import cartReducer from "./cartReducer";
 // Whenever an action is dispatched, Redux will update each top-level application state property
 // using the reducer with the matching name. It's important that the names match exactly, and that
 // the reducer acts on the corresponding ApplicationState property type.
-const roodReducer = combineReducers(
+const roorReducer = combineReducers(
     {
         cartState: cartReducer
     }
 );
 
-export default roodReducer;
+export default roorReducer;

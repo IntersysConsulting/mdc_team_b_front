bac37b3cf2283f41803b953a9958ccce597abc52
Christian <christianahvilla@gmail.com>
2019-09-06 14:59:11 -0500

    all comments are fixed

--- src/e-commerce/src/api/cartAPI.js
+++ src/e-commerce/src/api/cartAPI.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const getProducts = () => {
     return axios.get("").then(response => {
-        return response.data
+        response.data
     }).catch( error => {
-        return error.response
+        error.response
     })
 }
 

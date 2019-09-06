9fa305a553542de517dbf49b37149c90c84df7d9
Christian <christianahvilla@gmail.com>
2019-09-05 22:08:01 -0500

    install dependencies for react-router

--- src/e-commerce/src/index.js
+++ src/e-commerce/src/index.js
@@ -3,16 +3,11 @@ import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-import store from './store';
-import { Provider } from "react-redux";
-import Cart from './components/cart/cart';
+import Root from './components/root';
+import store from './store'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-        <Cart />
-    </Provider>, 
+    <Root store={store} />,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

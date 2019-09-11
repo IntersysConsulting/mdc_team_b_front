889456ac3e73834e4677979b118f333fe0224cb5
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-11 12:24:44 -0500

    Cart work in progress as a request for help

--- src/e-commerce/src/components/cart/cart.jsx
+++ src/e-commerce/src/components/cart/cart.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
 import "./cart.css";
 
-function Cart(props) {
-  const [value, setValue] = useState(props.value);
-
-  function display_text() {
-    if (value <= 0) {
-      return "";
-    } else if (value > 9) {
-      return "9+";
-    } else {
-      return value;
-    }
-  }
-
+const Cart = props => {
   return (
     <div className="cart-container" onClick={props.onClick}>
-      <h1
-        className="cart-text-holder"
-        className={"text-green cart-text " + (value > 9 ? "cart-text-ten" : "")}
-      >
-        {display_text()}
+      <h1 className={"text-green cart-text " + props.isOverNine}>
+        {props.displayText}
       </h1>
-      <p className={value > 9 ? "cart-tooltip" : "hidden"}>{props.value}</p>
+      <p className={props.isOverNine}>{props.tooltipText}</p>
     </div>
   );
-}
+};
 
 export default Cart;
+
+// Legacy code
+
+// const [cartState, setCartState] = useState({ value: props.value });
+
+// const display_text = () => {
+//   if (cartState.value <= 0) {
+//     return "";
+//   } else if (cartState.value > 9) {
+//     return "9+";
+//   } else {
+//     return cartState.value;
+//   }
+// };
+
+// return (
+//   <div className="cart-container" onClick={props.onClick}>
+//     <h1
+//       className={
+//         "text-green cart-text " + (cartState.value > 9 ? "cart-text-ten" : "")
+//       }
+//     >
+//       {display_text()}
+//     </h1>
+//     <p className={cartState.value > 9 ? "cart-tooltip" : "hidden"}>
+//       {cartState.value}
+//     </p>
+//   </div>
+// );
--- src/e-commerce/src/components/cart/demo.jsx
+++ src/e-commerce/src/components/cart/demo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Cart from "./cart.jsx";
+// import CartContainer as Cart from ".../container/cart/cart.js"; // This line doesn't quite work
 
-function CartDemo(props) {
-  function CartClick() {
+const CartDemo = props => {
+  const CartClick = () => {
     alert("You clicked on a cart!");
-  }
+  };
 
   const elements = [-100, 0, 1, 2, 3, 5, 6, 7, 8, 9, 12345];
   const items = [];
@@ -24,9 +25,13 @@ function CartDemo(props) {
   return (
     <div>
       <h1>Cart Demo</h1>
+      <h5>
+        Demo is currently broken because of container implementation (Review the
+        code of /components/cart/demo.jsx)
+      </h5>
       <div className="row">{items}</div>
     </div>
   );
-}
+};
 
 export default CartDemo;

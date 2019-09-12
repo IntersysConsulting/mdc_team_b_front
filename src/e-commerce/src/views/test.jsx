a445640a5788da5b31b07aefbffcb45766bb9415
Edwin Higareda <crhistianhigareda@gmail.com>
2019-09-12 10:14:06 -0500

    Deleted temporary testing code

--- src/e-commerce/src/views/test.jsx
+++ src/e-commerce/src/views/test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-import Description from '../components/product/description';
 
 function Test() {
   return (
@@ -10,7 +9,6 @@ function Test() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <p>{process.env.REACT_APP_API_URL}<code>src/App.js</code> and save to reload.</p>
-      <Description description = {'ME GUSTAN LAS PUTAS'}></Description>
     </div>
   );
 }

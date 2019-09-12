8db9f16713424975e56a1dd7a017a1bb7f9d390e
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-12 15:08:45 -0500

    Changed to Password Field

---
+++ src/e-commerce/src/components/password-field/password-field.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Form, InputGroup } from "react-bootstrap";
+import "./password-field.css";
+
+const PasswordField = props => {
+  return (
+    <div className="">
+      <InputGroup className="mb-4 justify-content-md-center">
+        <Form.Control
+          className="col-3 mr-4 "
+          type={props.open ? "text" : "password"}
+          placeholder="Password"
+        ></Form.Control>
+        <InputGroup.Append>
+          <div
+            className={
+              "toggle-visibility-button " +
+              (props.open ? "open-eye" : "closed-eye")
+            }
+            onClick={props.onClick}
+          ></div>
+        </InputGroup.Append>
+      </InputGroup>
+    </div>
+  );
+};
+
+export default PasswordField;
---
+++ src/e-commerce/src/containers/password-field/password-field.js
@@ -0,0 +1,14 @@
+import PasswordField from "../../components/password-field/password-field";
+import React, { useState } from "react";
+
+const PasswordFieldContainer = props => {
+    const [demoState, setDemoState] = useState({ isOpen: false });
+
+    const ToggleVisibility = () => {
+        setDemoState({ isOpen: !demoState.isOpen });
+    };
+    return <PasswordField open={demoState.isOpen}
+        onClick={ToggleVisibility}></PasswordField>;
+};
+
+export default PasswordFieldContainer;

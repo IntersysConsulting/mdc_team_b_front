662a0d0268cad8c272519e37a5d788c5b936198f
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-13 09:47:46 -0500

    Updated demo page

--- src/e-commerce/src/App.js
+++ src/e-commerce/src/App.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
-import DemoButton from './demos/demo-button';
-import DemoStatus from './demos/demo-status';
-import './App.css';
-
+import React, { useState } from "react";
+import DemoButton from "./demos/demo-button";
+import DemoStatus from "./demos/demo-status";
+import DemoCards from "./demos/demo-cards";
+import "./App.css";
 
 const App = () => {
   const defaultAlerts = true;
-  const [appState, setAppState] = useState({alerts: defaultAlerts});
+  const [appState, setAppState] = useState({ alerts: defaultAlerts });
 
   return (
     <div className="App">
       <DemoButton></DemoButton>
       <DemoStatus></DemoStatus>
+      <DemoCards></DemoCards>
     </div>
   );
 };
---
+++ src/e-commerce/src/demos/demo-cards.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import PersonalDetailsCardDemo from "./components/personal-details-card/demo.jsx";
+
+const DemoCards = () => {
+  return (
+    <div className="container">
+      <PersonalDetailsCardDemo></PersonalDetailsCardDemo>
+    </div>
+  );
+};
+
+export default DemoCards;

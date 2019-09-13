5893544bc3f9e31f1ba351c2fb9d42fa131a4ba0
Edwin Higareda <crhistianhigareda@gmail.com>
2019-09-13 11:05:31 -0500

    Added color and adjusted main size

--- src/e-commerce/src/components/checkbox/Checkbox.jsx
+++ src/e-commerce/src/components/checkbox/Checkbox.jsx
@@ -1,58 +1,38 @@
 // Example of how to insert this component:
 // import Checkbox from '../components/checkbox/Checkbox';
 // <Checkbox text = "Are you human?" />
-//
 // This checkbox should return a TRUE or FALSE statement
 
-import React from 'react';
+import React, {useState} from 'react';
+import './checkbox.css';
+import '../../custom.scss';
 
-class Checkbox extends React.Component {
-    constructor(props) {
-        super(props);
-        /* set the initial checkboxState to false, which means: unchecked */
-        this.state = {
-            checkboxState: false
-        }
-    }
-
-
-    /* prevent form submission from reloading the page */
-    onSubmit(event) {
-        event.preventDefault();
-    }
-
-
+export default function Checkbox(props) {
+    const [checkboxState, setCheckboxState] = useState(false);
+    
     /* callback to change the checkboxState to the opposite 
     boolean value when the checkbox is checked or unchecked */
-    toggle(event) {
-        this.setState({
-            checkboxState: !this.state.checkboxState
-        });
+    function handleCheckboxStateChange(e) {
+        setCheckboxState(!checkboxState);
     }
 
-
-    render() {
-
         const checkbox = (
-            <span>
-                <input
-                    type="checkbox"
-                    id={`react-switch-new`}
-                    onClick={this.toggle.bind(this)}
-                />
-                <label>{this.props.text}</label>
-                {/* <p>{this.state.checkboxState.toString()}</p> */}
-            </span>
+            <label className="container">{props.text}
+                    <input
+                        type="checkbox"
+                        id={`react-switch-new`}
+                        onClick={handleCheckboxStateChange}
+                    />
+                    <span className="checkmark"></span>
+                    {/* Uncomment the next line to test if the value is being changed */}
+                    {/* <p>{checkboxState.toString()}</p> */}
+            </label>
         );
 
         return (
             <div>
                 {checkbox}
-                {this.state.checkboxState}
+                {checkboxState}
             </div>
         );
-
-    }
 }
-
-export default Checkbox
--- src/e-commerce/src/views/test.jsx
+++ src/e-commerce/src/views/test.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import Checkbox from '../components/checkbox/Checkbox';
-import Description from '../components/product/description';
-import Switch from '../components/physical-digital/Switch'
 
 function Test() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Switch/>
-      <Description description = "Esta es una descripcion" />
+    <div>
       <Checkbox text = "Are you human?"/>
-      <p>{process.env.REACT_APP_API_URL}<code>src/App.js</code> and save to reload.</p>
     </div>
   );
 }

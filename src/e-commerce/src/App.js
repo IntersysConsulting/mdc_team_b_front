7d5565b08eecd66f5f55844ac815456f23c5d2cc
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-13 14:10:15 -0500

    Linter fixes

--- src/e-commerce/src/App.js
+++ src/e-commerce/src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import DemoButton from "./demos/demo-button";
-import DemoStatus from "./demos/demo-status";
-import DemoAddress from "./demos/demo-address";
-import "./App.css";
+import React, {useState} from 'react';
+import DemoButton from './demos/demo-button';
+import DemoStatus from './demos/demo-status';
+import DemoAddress from './demos/demo-address';
+import './App.css';
 
 const App = () => {
   const defaultAlerts = true;
-  const [appState, setAppState] = useState({ alerts: defaultAlerts });
+  const [appState, setAppState] = useState({alerts: defaultAlerts});
 
   return (
     <div className="App">
--- src/e-commerce/src/demos/demo-address.js
+++ src/e-commerce/src/demos/demo-address.js
@@ -1,5 +1,5 @@
-import React from "react";
-import AddressManagementCardDemo from "../components/address-management-card/demo.jsx";
+import React from 'react';
+import AddressManagementCardDemo from '../components/address-management-card/demo.jsx';
 
 const DemoAddress = () => {
   return (

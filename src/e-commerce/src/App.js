7e26514ea0854dca38fee8b4089aed07001cd7d0
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-13 14:09:54 -0500

    Rebased on master to solve issues

--- src/e-commerce/src/App.js
+++ src/e-commerce/src/App.js
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
-import DemoButton from './demos/demo-button';
-import DemoStatus from './demos/demo-status';
-import './App.css';
+import React, { useState } from "react";
+import DemoButton from "./demos/demo-button";
+import DemoStatus from "./demos/demo-status";
+import DemoAddress from "./demos/demo-address";
+import "./App.css";
 
 const App = () => {
   const defaultAlerts = true;
-  const [appState, setAppState] = useState({alerts: defaultAlerts});
+  const [appState, setAppState] = useState({ alerts: defaultAlerts });
 
   return (
     <div className="App">
       <DemoButton></DemoButton>
       <DemoStatus></DemoStatus>
+      <DemoAddress></DemoAddress>
     </div>
   );
 };

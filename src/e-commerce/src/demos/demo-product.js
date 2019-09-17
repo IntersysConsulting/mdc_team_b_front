98ee5564fde0af5efb829bc99132c917125082f7
Christian <christianahvilla@gmail.com>
2019-09-17 12:14:12 -0500

    Add image containter and demo for image

---
+++ src/e-commerce/src/demos/demo-product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DemoStorefrontImage from '../components/storefrontImage/demo';
+
+const DemoProduct = () => {
+    return (
+        <div className="container">
+            <DemoStorefrontImage></DemoStorefrontImage>
+        </div>
+    );
+};
+
+export default DemoProduct;

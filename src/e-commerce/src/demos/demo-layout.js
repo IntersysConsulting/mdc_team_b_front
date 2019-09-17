43e4e89375ba459993d64d0ab1f35fb11f885fc3
Ana Sandoval <asandoval@intersysconsulting.com>
2019-09-17 11:30:18 -0500

    Added logo component
    Currently redirects to demo page when clicked, ideal behavior is for it to redirect to frontstore once that view is ready.

--- src/e-commerce/src/demos/demo-layout.js
+++ src/e-commerce/src/demos/demo-layout.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import SearchBar from '../components/search-bar/search-bar';
+import Logo from '../components/logo/logo';
 
 const LayoutDemo = () => {
   return (
     <div>
       <h1>Demo Layout Components</h1>
       <SearchBar></SearchBar>
+      <Logo link="/"/>
     </div>
   );
 };

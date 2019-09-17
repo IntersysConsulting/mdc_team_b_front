685e3f8085717e1abe67f067b92f29a36a89a755
Christian <christianahvilla@gmail.com>
2019-09-17 12:15:44 -0500

    Add demo

--- src/e-commerce/src/demos/demo-product.js
+++ src/e-commerce/src/demos/demo-product.js
@@ -2,11 +2,11 @@ import React from 'react';
 import DemoStorefrontImage from '../components/storefrontImage/demo';
 
 const DemoProduct = () => {
-    return (
-        <div className="container">
-            <DemoStorefrontImage></DemoStorefrontImage>
-        </div>
-    );
+  return (
+    <div className="container">
+      <DemoStorefrontImage></DemoStorefrontImage>
+    </div>
+  );
 };
 
 export default DemoProduct;

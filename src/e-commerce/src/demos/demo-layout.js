421f384501920bcd0e3b233d7ace09f52a420846
Ana Sandoval <asandoval@intersysconsulting.com>
2019-09-17 12:56:52 -0500

    Added blocker for views
    Blocker is needed for sidebar behavior in layout. For the purpose of the demo, a button is implemented which when clicked opens the blocker; to close it, just click anywhere over it.

--- src/e-commerce/src/demos/demo-layout.js
+++ src/e-commerce/src/demos/demo-layout.js
@@ -1,16 +1,30 @@
 import React, {useState} from 'react';
 import SearchBar from '../components/search-bar/search-bar';
 import Logo from '../components/logo/logo';
+import Blocker from '../components/blocker/blocker';
+import Button from '../components/button/button';
 
 const LayoutDemo = () => {
+  const [blockerState, setBlockerState] = useState({show: false});
+
+  const BlockerHandler = () => {
+    console.log(blockerState.show);
+    setBlockerState({show: !blockerState.show});
+  };
+
+  const closed = () => {
+    setBlockerState({show: !blockerState.show});
+  };
+
   return (
     <div>
       <h1>Demo Layout Components</h1>
       <SearchBar></SearchBar>
       <Logo link="/"/>
+      <br/><Button onClick={BlockerHandler}>Press to block view</Button>
+      <Blocker show = {blockerState.show} clicked={closed} ></Blocker>
     </div>
   );
 };
 
 export default LayoutDemo;
-

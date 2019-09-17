f0a6744b51b8ca1ddcc3cc08eba3f25b3beca357
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-17 10:12:36 -0500

    Started work

---
+++ src/e-commerce/src/components/checkout-table/checkout-table.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./checkout-table.css";
+import { Table } from "react-bootstrap";
+
+const CheckoutTable = props => {
+  const MakeRows = () => {};
+
+  <div className="container"></div>;
+};
+
+export default CheckoutTable;
---
+++ src/e-commerce/src/components/checkout-table/demo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CheckoutTable from "./checkout-table.jsx";
+
+const CheckoutTableDemo = props => {
+  return (
+    <div>
+      <CheckoutTable></CheckoutTable>
+    </div>
+  );
+};
+
+export default CheckoutTableDemo;

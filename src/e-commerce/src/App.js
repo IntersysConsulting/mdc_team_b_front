7cf2aaaa362b7fcfef5f705f3acc9202803e56ba
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-18 10:11:42 -0500

    Started work on modal

--- src/e-commerce/src/App.js
+++ src/e-commerce/src/App.js
@@ -1,29 +1,41 @@
-import React, {useState} from 'react';
-import DemoButton from './demos/demo-button';
-import DemoStatus from './demos/demo-status';
-import DemoCards from './demos/demo-cards';
-import DemoPrice from './demos/demo-price';
-import DemoQuantity from './demos/demo-quantity';
-import DemoLayout from './demos/demo-layout';
-import DemoCheckout from './demos/demo-checkout';
-import DemoLogin from './demos/demo-login';
-import DemoSwitch from './demos/demo-switch';
-import CheckboxDemo from './demos/demo-checkbox';
-import DemoProduct from './demos/demo-product';
-import LayoutDemo from './demos/demo-layout'
+import React, { useState } from "react";
+import DemoButton from "./demos/demo-button";
+import DemoStatus from "./demos/demo-status";
+import DemoCards from "./demos/demo-cards";
+import DemoPrice from "./demos/demo-price";
+import DemoQuantity from "./demos/demo-quantity";
+import DemoLayout from "./demos/demo-layout";
+import DemoCheckout from "./demos/demo-checkout";
+import DemoLogin from "./demos/demo-login";
+import DemoSwitch from "./demos/demo-switch";
+import CheckboxDemo from "./demos/demo-checkbox";
+import DemoProduct from "./demos/demo-product";
 
+import DemoModals from "./demos/demo-modals.js";
+
+import "./App.css";
 const App = () => {
   const defaultAlerts = true;
-  const [appState, setAppState] = useState({alerts: defaultAlerts});
+  const [appState, setAppState] = useState({ alerts: defaultAlerts });
 
   return (
     <div className="App">
-      <DemoLayout/>
-      
-      <DemoCards/>
-      <DemoCheckout/>
-      <DemoProduct/>
-      
+      <DemoLayout />
+
+      <DemoCards />
+      <DemoCheckout />
+      <DemoProduct />
+
+      <DemoModals></DemoModals>
+      <DemoLogin></DemoLogin>
+      <DemoButton></DemoButton>
+      <DemoStatus></DemoStatus>
+
+      <DemoPrice></DemoPrice>
+      <DemoQuantity alerts={appState.alerts}></DemoQuantity>
+
+      <DemoSwitch />
+      <CheckboxDemo />
     </div>
   );
 };

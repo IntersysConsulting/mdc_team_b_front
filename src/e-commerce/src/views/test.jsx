b2239ed3b2e1a8bf56354aa54048578dbb4b6c44
Edwin Higareda <crhistianhigareda@gmail.com>
2019-09-19 15:41:22 -0500

    Finished Switch Component

--- src/e-commerce/src/views/test.jsx
+++ src/e-commerce/src/views/test.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Switch from '../components/2WordSwitch/Switch'
 import '../App.css';
 
 function Test() {
   return (
-    <div className="col-1">
-      <Switch leftWord = "Physical" rightWord = "Digital"/>
+    <div >
     </div>
   );
 }

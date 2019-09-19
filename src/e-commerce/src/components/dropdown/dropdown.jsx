de07ea5e02ec5b07c1aeb08ffcd50242180d3e23
Alfredo Sanchez <advluissanchez@gmail.com>
2019-09-19 17:34:46 -0500

    Changes

--- src/e-commerce/src/components/dropdown/dropdown.jsx
+++ src/e-commerce/src/components/dropdown/dropdown.jsx
@@ -1,20 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const Dropdown = (props) => {    
-    const [ddItem, setDditem] = useState('')
-
     return(
-        <div className="dd-wrapper">
-            <div className="dd-header">
-                    <select className='dd-list' name={props.title}>
-                        {props.list.map((item) => (
-                            <option  className='dd-list-item' key={item.id}>
-                                {item.title}
-                            </option>
-                        ))}
-                    </select>
-            </div>
-        </div>
+        <select>
+            {props.list.map((item) => (
+                <option  key={item.id}>
+                    {item.title}
+                </option>
+            ))}
+        </select>
     )
 }
 
@@ -27,20 +21,14 @@ export default Dropdown;
         {
             id: 0,
             title: 'New York',
-            selected: false,
-            key: 'location'
         },
         {
             id: 1,
             title: 'Dublin',
-            selected: false,
-            key: 'location'
         },
         {
             id: 2,
             title: 'California',
-            selected: false,
-            key: 'location'
         }
     ]
 

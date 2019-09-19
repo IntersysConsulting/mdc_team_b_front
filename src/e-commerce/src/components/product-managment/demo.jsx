5ead61792819bbf706ab2f147c2e0b52fde805ea
Christian <christianahvilla@gmail.com>
2019-09-19 11:58:22 -0500

    Add product card

--- src/e-commerce/src/components/product-managment/demo.jsx
+++ src/e-commerce/src/components/product-managment/demo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CardProduct from "./card.jsx";
+import CardProductManagment from "./product-managment-card.jsx";
 import digital from '../../assets/images/digital.jpg';
 import not from '../../assets/images/not.jpg';
 import physical from '../../assets/images/physical.jpg';
@@ -8,12 +8,12 @@ import bears from '../../assets/images/bears.jpg';
 const ProductManagmentDemo = props => {
     return (
         <div className="container">
-            <h1>Product Card Demo</h1>
+            <h1>Product Managment Card Demo</h1>
             <div className="row justify-content-md-center mx-auto">
-                <CardProduct price={3} url={digital} name={"Gears of War 5"}></CardProduct>
-                <CardProduct price={20000} url={not} name={"Movie"}></CardProduct>
-                <CardProduct price={150000} url={physical} name={"Rubik Cube"}></CardProduct>
-                <CardProduct price={150000} url={bears} name={"Bear"}></CardProduct>
+                <CardProductManagment price={3} url={digital} name={"Gears of War 5"}></CardProductManagment>
+                <CardProductManagment price={20000} url={not} name={"Movie"}></CardProductManagment>
+                <CardProductManagment price={150000} url={physical} name={"Rubik Cube"}></CardProductManagment>
+                <CardProductManagment price={150000} url={bears} name={"Bear"}></CardProductManagment>
             </div>
         </div>
     );

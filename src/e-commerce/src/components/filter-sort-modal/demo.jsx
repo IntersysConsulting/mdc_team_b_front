5bca0f1537c8beda6a7ab3566d463fe042e936fd
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-23 15:18:15 -0500

    Implemented filter-sort modal

--- src/e-commerce/src/components/filter-sort-modal/demo.jsx
+++ src/e-commerce/src/components/filter-sort-modal/demo.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import FilterSortModal from "./filter-sort-modal.jsx";
+import { Button } from "react-bootstrap";
 
 const FilterModalDemo = props => {
+  // This was also integrated in search-bar.js
+  const [demoState, setDemoState] = useState({ show: false });
+
+  const filterOptions = ["Bears", "Dogs", "Horses", "Donkeys"];
+
+  const handleClose = () => {
+    setDemoState({ show: false });
+  };
+
+  const handleAccept = () => {
+    // Do something like access the API for a filter/sort
+    setDemoState({ show: false });
+  };
+
+  const handleOpen = () => {
+    setDemoState({ show: true });
+  };
+
   return (
-    <div>
+    <div className="justify-content-center">
       <h1>Filter Modal</h1>
-      <FilterSortModal></FilterSortModal>
+      <Button variant="warning" onClick={handleOpen}>
+        Filter
+      </Button>
+      <FilterSortModal
+        filters={filterOptions}
+        handleCloseModal={handleClose}
+        handleAcceptModal={handleAccept}
+        show={demoState.show}
+      ></FilterSortModal>
     </div>
   );
 };
--- src/e-commerce/src/components/filter-sort-modal/filter-sort-modal.jsx
+++ src/e-commerce/src/components/filter-sort-modal/filter-sort-modal.jsx
@@ -1,54 +1,154 @@
-import React, { useState } from "react";
-import { Modal, Form, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Modal, Form, Row, Col, Spinner } from "react-bootstrap";
 import "./filter-sort-modal.css";
 import ChangeViewButton from "../change-view-button/change-view-button.jsx";
 import RadioButton from "../radio-button/radio-button.jsx";
 import CancelButton from "../cancel-button/cancel-button.jsx";
+import Checkbox from "../checkbox/Checkbox.jsx";
 import Switch from "../2WordSwitch/Switch.jsx";
 
 const FilterSortModal = props => {
+  const radioIds = ["a-z", "z-a", "cheapest", "priciest", "newest", "oldest"];
+  var Checkboxes = [];
   const [modalState, setModalState] = useState({
-    sort: "oldest",
-    filter: "none",
-    selectedSort: true
+    sort: radioIds[5],
+    filter: undefined,
+    isSortMenuSelected: false,
+    isLoading: true,
+    minPrice: 0,
+    maxPrice: -1,
+    show: false
   });
 
-  const radioIds = ["a-z", "z-a", "cheapest", "priciest", "newest", "oldest"];
+  // ComponentDidMount
+  useEffect(() => {
+    var filterList = [];
+    props.filters.forEach((value, index, array) => {
+      filterList.push({ id: "checkbox-" + value, checked: true });
+    });
+    setModalState({
+      sort: modalState.sort,
+      filter: filterList,
+      isSortMenuSelected: modalState.isSortMenuSelected,
+      isLoading: false,
+      minPrice: modalState.minPrice,
+      maxPrice: modalState.maxPrice,
+      show: props.show
+    });
+  }, []);
 
-  const filterBody = () => {
-    return <div></div>;
-  };
+  //#region Variants of SetState
 
   const setSortState = e => {
     setModalState({
       sort: e.target.id,
       filter: modalState.filter,
-      selectedSort: modalState.selectedSort
+      isSortMenuSelected: modalState.isSortMenuSelected,
+      isLoading: modalState.isLoading,
+      minPrice: modalState.minPrice,
+      maxPrice: modalState.maxPrice,
+      show: props.show
     });
   };
 
   const setFilterState = e => {
     setModalState({
       sort: modalState.sort,
-      filter: e.target.filter,
-      selectedSort: modalState.selectedSort
+      filter: e,
+      isSortMenuSelected: modalState.isSortMenuSelected,
+      isLoading: modalState.isLoading,
+      minPrice: modalState.minPrice,
+      maxPrice: modalState.maxPrice,
+      show: props.show
     });
   };
 
   const flipSwitch = e => {
-    console.log("Switch was hit. E= " + e);
+    console.log("Switch button was clicked");
     setModalState({
       sort: modalState.sort,
       filter: modalState.filter,
-      selectedSort: !modalState.selectedSort
+      isSortMenuSelected: !modalState.isSortMenuSelected,
+      isLoading: modalState.isLoading,
+      minPrice: modalState.minPrice,
+      maxPrice: modalState.maxPrice,
+      show: props.show
+    });
+  };
+
+  const setMinimumPrice = e => {
+    setModalState({
+      sort: modalState.sort,
+      filter: modalState.filter,
+      isSortMenuSelected: modalState.isSortMenuSelected,
+      isLoading: modalState.isLoading,
+      minPrice: e.target.value,
+      maxPrice: modalState.maxPrice,
+      show: props.show
+    });
+  };
+
+  const setMaximumPrice = e => {
+    setModalState({
+      sort: modalState.sort,
+      filter: modalState.filter,
+      isSortMenuSelected: modalState.isSortMenuSelected,
+      isLoading: modalState.isLoading,
+      minPrice: modalState.minPrice,
+      maxPrice: e.target.value,
+      show: props.show
     });
   };
 
   const clearAll = () => {
-    setModalState({});
+    var cleanFilter = modalState.filter;
+    cleanFilter.forEach((value, index, array) => {
+      value.checked = props.show;
+    });
+
+    setModalState({
+      sort: radioIds[5],
+      filter: cleanFilter,
+      isSortMenuSelected: modalState.isSortMenuSelected,
+      isLoading: modalState.isLoading,
+      minPrice: 0,
+      maxPrice: -1,
+      show: props.show
+    });
   };
 
+  //#endregion
+
+  //#region Checkboxes
+  const makeCheckboxes = () => {
+    props.filters.forEach((value, index, array) => {
+      Checkboxes.push(
+        <Checkbox
+          text={value}
+          id={modalState.filter[index].id}
+          checked={modalState.filter[index].checked}
+          onClick={handleCheckboxMark}
+        />
+      );
+    });
+  };
+  const handleCheckboxMark = e => {
+    console.log(e.target);
+    const target = e.target.id;
+    var filter = modalState.filter;
+    filter.forEach((value, index, array) => {
+      if (value.id === target) {
+        console.log(value.id + " " + index + " " + value.checked);
+        value.checked = !value.checked;
+      }
+    });
+    setFilterState(filter);
+  };
+  //#endregion Checkboxes
+
+  //#region Bodies of the modal
   const sortBody = () => {
+    // The body when Sort is selected
     return (
       <Form>
         <Form.Row className="text-left">
@@ -58,21 +158,21 @@ const FilterSortModal = props => {
               label="A-Z"
               id={radioIds[0]}
               onChange={setSortState}
-              checked={modalState.sort == radioIds[0]}
+              checked={modalState.sort === radioIds[0]}
             ></RadioButton>
             <RadioButton
               name="sort-radios"
               label="Lowest Price"
               id={radioIds[2]}
               onChange={setSortState}
-              checked={modalState.sort == radioIds[2]}
+              checked={modalState.sort === radioIds[2]}
             ></RadioButton>
             <RadioButton
               name="sort-radios"
               label="Newest"
               id={radioIds[4]}
               onChange={setSortState}
-              checked={modalState.sort == radioIds[4]}
+              checked={modalState.sort === radioIds[4]}
             ></RadioButton>
           </Form.Group>
           <Form.Group as={Col}>
@@ -81,21 +181,21 @@ const FilterSortModal = props => {
               label="Z-A"
               id={radioIds[1]}
               onChange={setSortState}
-              checked={modalState.sort == radioIds[1]}
+              checked={modalState.sort === radioIds[1]}
             ></RadioButton>
             <RadioButton
               name="sort-radios"
               label="Highest Price"
               id={radioIds[3]}
               onChange={setSortState}
-              checked={modalState.sort == radioIds[3]}
+              checked={modalState.sort === radioIds[3]}
             ></RadioButton>
             <RadioButton
               name="sort-radios"
               label="Oldest"
               id={radioIds[5]}
               onChange={setSortState}
-              checked={modalState.sort == radioIds[5]}
+              checked={modalState.sort === radioIds[5]}
             ></RadioButton>
           </Form.Group>
         </Form.Row>
@@ -103,32 +203,80 @@ const FilterSortModal = props => {
     );
   };
 
+  const filterBody = () => {
+    return (
+      <Form>
+        {modalState.isLoading ? (
+          <Spinner animation="border" variant="warning" />
+        ) : (
+          makeCheckboxes()
+        )}
+        <Form.Row className="text-left pl-1">
+          <Form.Group as={Col} className="filter-body">
+            {Checkboxes}
+          </Form.Group>
+        </Form.Row>
+        <h1>Price</h1>
+        <Row>
+          <Col>
+            <Form.Control
+              type="text"
+              placeholder="Min"
+              value={modalState.minPrice == 0 ? "" : modalState.minPrice} // Checks if the value is the default, then ignores for display
+              onChange={setMinimumPrice}
+            ></Form.Control>
+          </Col>
+          <Col>
+            <Form.Control
+              type="text"
+              placeholder="Max"
+              value={
+                modalState.maxPrice == -1 // Checks if the value is the default, then ignores for display
+                  ? ""
+                  : modalState.maxPrice
+              }
+              onChange={setMaximumPrice}
+            ></Form.Control>
+          </Col>
+        </Row>
+      </Form>
+    );
+  };
+  //#endregion
+
   return (
-    <div>
-      <Modal.Dialog className="filter-sort-modal">
+    <div className="row">
+      <Modal
+        dialogClassName="filter-sort-modal "
+        show={props.show}
+        onHide={props.handleCloseModal}
+        centered
+      >
         <Modal.Header>
           <Modal.Title>
             <Switch
               leftWord="Filter"
               rightWord="Sort"
-              isOn={modalState.selectedSort}
+              isOn={modalState.isSortMenuSelected}
               onClick={flipSwitch}
             ></Switch>
           </Modal.Title>
-          <CancelButton className="filter-sort-clear-button">
+          <CancelButton className="filter-sort-clear-button" onClick={clearAll}>
             Clear
           </CancelButton>
         </Modal.Header>
-        {/* Here a function based on what title is selected shows different options */}
         <Modal.Body className="filter-sort-modal-body-text">
-          {sortBody()}
+          {modalState.isSortMenuSelected ? sortBody() : filterBody()}
         </Modal.Body>
         <Modal.Footer>
-          <ChangeViewButton className="col-12 filter-sort-modal-button">
+          <ChangeViewButton
+            className="col-12 filter-sort-modal-button"
+            onClick={props.handleAcceptModal}
+          >
             Apply
           </ChangeViewButton>
         </Modal.Footer>
-      </Modal.Dialog>
+      </Modal>
     </div>
   );
 };

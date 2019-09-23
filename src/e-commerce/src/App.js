dddf42c74b7df1675a42955e286e70adfe41c6c2
Ana Sandoval <asandoval@intersysconsulting.com>
2019-09-23 12:56:02 -0500

    Demo files updated
    If you wish to see your components alongside the layout they should now be included in the demo-layout file, and layout is the only component included in app.js for the moment.

--- src/e-commerce/src/App.js
+++ src/e-commerce/src/App.js
@@ -5,25 +5,18 @@ import DemoCards from './demos/demo-cards';
 import DemoPrice from './demos/demo-price';
 import DemoQuantity from './demos/demo-quantity';
 import DemoLayout from './demos/demo-layout';
-import DemoCheckout from './demos/demo-checkout';
 import DemoLogin from './demos/demo-login';
 import DemoSwitch from './demos/demo-switch';
 import CheckboxDemo from './demos/demo-checkbox';
 import DemoProduct from './demos/demo-product';
-import LayoutDemo from './demos/demo-layout'
+import LayoutCustomer from './components/layout/layout-customer';
+import LayoutAdmin from './components/layout/layout-admin';
 
 const App = () => {
-  const defaultAlerts = true;
-  const [appState, setAppState] = useState({alerts: defaultAlerts});
 
   return (
     <div className="App">
-      <DemoLayout/>
-      
-      <DemoCards/>
-      <DemoCheckout/>
-      <DemoProduct/>
-      
+      <DemoLayout></DemoLayout>
     </div>
   );
 };

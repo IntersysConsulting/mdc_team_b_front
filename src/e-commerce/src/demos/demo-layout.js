8a74d3341f6b587f5907ee486d6dddcd0cca233c
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-23 15:17:49 -0500

    Pre-linter

--- src/e-commerce/src/demos/demo-layout.js
+++ src/e-commerce/src/demos/demo-layout.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
-import LayoutCustomer from '../components/layout-customer/layout-customer';
+import React, { useState } from "react";
+import LayoutCustomer from "../components/layout-customer/layout-customer";
 
 const LayoutDemo = () => {
-  const [accessLevelState, setAccessLevelState] = useState({ 
+  const [accessLevelState, setAccessLevelState] = useState({
     accesses: [
-        {role: 'registeredUser', name: 'John Smith'},
-        {role: 'guest', name: 'Guest'}
-    ],
+      { role: "registeredUser", name: "John Smith" },
+      { role: "guest", name: "Guest" }
+    ]
   });
 
-  return(<LayoutCustomer accessLevelState = {accessLevelState} />);
+  return <LayoutCustomer accessLevelState={accessLevelState} />;
 };
 
 export default LayoutDemo;

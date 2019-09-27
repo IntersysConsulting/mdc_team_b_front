44da5bd32bbb0f639270618a461d54ea8659bd62
Pablo Rivera <privera@intersysconsulting.com>
2019-09-27 11:39:17 -0500

    deleted mozilla additional number arrows, added debouncer for quantity component, had to turn the component into a class

--- src/components/quantity/quantity.jsx
+++ src/components/quantity/quantity.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "./quantity.css";
+import { setInterval } from "timers";
 
 const QuantityButton = props => {
   return (
@@ -13,55 +14,108 @@ const QuantityButton = props => {
   );
 };
 
-const Quantity = props => {
-  const [quantityState, setQuantityState] = useState({ value: 1 });
+class Quantity extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 1,
+      isDebouncing: false,
+      debounceTimer: 0
+    }
+    setInterval(()=>{
+      if(this.state.isDebouncing){
+        if(this.state.debounceTimer < 800 ){
+          this.setState({
+            debounceTimer: this.state.debounceTimer+100
+          });
+        }
+        else{
+          this.props.onChange();
+          this.setState({
+            isDebouncing:false,
+            debounceTimer: 0
+          })
+        }
+      }
+    },100)
+  }
 
-  const decrease = () => {
-    let val = quantityState.value;
-    val = isNaN(val) ? 0 : val;
+  decrease = () => {
+    this.setState({
+      isDebouncing: true,
+      debounceTimer: 0
+    });
+
+    let val = this.state.value;
+    if(typeof(val) !== 'number') {
+      val = 0;
+    }
     val < 1 ? (val = 0) : (val--);
-    setQuantityState({ value: val });
-    props.onChange();
+    if(val === 0) {
+      alert("You can't have 0 of a product, use delete instead");
+      val = 1;
+    }
+    this.setState({ value: val });
   };
 
-  const increase = () => {
-    let val = quantityState.value;
-    val = isNaN(val) ? 0 : val;
-    val++;
-    setQuantityState({ value: val });
-    props.onChange();
+  increase = () => {
+    this.setState({
+      isDebouncing: true,
+      debounceTimer: 0
+    });
+
+    let val = this.state.value;
+    if(typeof(val) !== 'number') {
+      val = 1;
+    }
+    val > 99999 ? (val = 99999) : (val++);
+    this.setState({ value: val });
   };
 
-  const changeVal = event => {
-    let val = event.target.value;
-    val = isNaN(val) ? 0 : val;
-    setQuantityState({ value: val });
-    props.onChange();
+  changeVal = event => {
+    this.setState({
+      isDebouncing: true,
+      debounceTimer: 0
+    });
+
+    let val = parseInt(event.target.value, 10);
+    if(isNaN(val) || val === 0) {
+      alert("You can't have 0 of a product, use delete instead");
+      val = 1;
+    }
+    if(val > 99999)
+      val = 99999;
+    else if (val < 0)
+      val = 0;
+    this.setState({ value: val });
   };
 
-  return (
-    <div className="quantity-container">
-      <QuantityButton
-        className="value-button-left bg-dark  text-white"
-        idVal="decrease"
-        symbol="-"
-        onClick={decrease}
-      />
-      <input
-        className="border-dark border-top border-bottom"
-        type="number"
-        id="number"
-        value={quantityState.value}
-        onChange={changeVal}
-      />
-      <QuantityButton
-        className="value-button-right bg-dark text-white"
-        idVal="increase"
-        symbol="+"
-        onClick={increase}
-      />
-    </div>
-  );
+  render() {
+    return (
+      <div className="quantity-container">
+        <QuantityButton
+          className="value-button-left bg-dark  text-white"
+          idVal="decrease"
+          symbol="-"
+          onClick={this.decrease}
+        />
+        <input
+          className="border-dark border-top border-bottom"
+          type="number"
+          id="number"
+          value={this.state.value}
+          onChange={this.changeVal}
+        />
+        <QuantityButton
+          className="value-button-right bg-dark text-white"
+          idVal="increase"
+          symbol="+"
+          onClick={this.increase}
+        />
+      </div>
+    );
+  }
+
 };
 
 export default Quantity;

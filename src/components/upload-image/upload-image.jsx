553a3f5f7a3c6263291ee9fb943470ba41804053
Christian <christianahvilla@gmail.com>
2019-10-08 10:27:27 -0500

    Add upload-image component

---
+++ src/components/upload-image/upload-image.jsx
@@ -0,0 +1,31 @@
+// Example of how to insert this component:
+// import Checkbox from '../components/checkbox/Checkbox';
+// <Checkbox text = "Are you human?" />
+// This checkbox should return a TRUE or FALSE statement
+
+import React, { useState, useEffect } from "react";
+import "../../custom.scss";
+
+const UploadImage = props => {
+    
+    const [checkboxState, setCheckboxState] = useState({
+    checked: props.checked
+    });
+
+    useEffect(
+        e => {
+        setCheckboxState({ checked: props.checked });
+        },
+        [props.checked]
+    );
+
+    return (
+        <label className={"checkbox-container " + props.className}>
+            {props.children}
+            <input name={props.name} type="checkbox" id={props.id} onClick={props.onClick} defaultChecked={checkboxState.checked}/>
+            <span className="checkmark"></span>
+        </label>
+    );
+};
+
+export default UploadImage;

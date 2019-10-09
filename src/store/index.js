adf6e4862e390d3086d471b2890b714c8b3f0152
Edwin Higareda <crhistianhigareda@gmail.com>
2019-10-09 11:34:32 -0500

    delete

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from '../reducers';
-import thunk from 'redux-thunk';
-import devToolsEnhancer from 'remote-redux-devtools';
-
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), devToolsEnhancer()))
-
-export default store;
